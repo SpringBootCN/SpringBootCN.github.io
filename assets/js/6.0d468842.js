b451db656a17fbedb3badb5688edabe03305669d
DHbin <admin@spring-boot.cn>
2021-03-10 02:31:14 +0000

    generate static resources, triggerd by travis ci

---
+++ assets/js/6.0d468842.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{208:function(t,n,s){"use strict";s.r(n);var e=s(0),o=Object(e.a)({},(function(){var t=this.$createElement,n=this._self._c||t;return n("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[n("p",[this._v("邮箱: admin@spring-boot.cn")])])}),[],!1,null,null,null);n.default=o.exports}}]);
